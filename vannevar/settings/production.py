e943086fa306ce01bd270bcc9de008749bcf13e4
Allan Berry <allan.berry@gmail.com>
2016-06-09 07:46:46 -0500

    Fatfinger

--- vannevar/settings/production.py
+++ vannevar/settings/production.py
@@ -28,4 +28,4 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
     'compressor.filters.cssmin.CSSMinFilter',
 ]
-COMPRESS_CSS_HASHING_METHOD = 'content
+COMPRESS_CSS_HASHING_METHOD = 'content'
