ad7bbc5d5092a40fe5be04fe99287d128afd1018
Allan Berry <allan.berry@gmail.com>
2016-07-26 08:26:37 -0500

    Working on help text, and fleshing out some model details.

--- collection/models.py
+++ collection/models.py
@@ -11,6 +11,9 @@ from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKe
 
 
 class Base(models.Model):
+    '''
+    The most rudimentary stuff, for use in every custom model.
+    '''
     class Meta:
         abstract = True
 
@@ -31,17 +34,24 @@ class Base(models.Model):
 
 
 class Item(Base):
+    '''
+    The fundamental class connecting to attributes.
+    '''
     class Meta:
         abstract = True
 
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField(max_length=200,
+        help_text="Work from the general to the specific, e.g. <em>last-name-first-name</em>.",
+    )
 
     # fields for (receiving) generic relations
     dates = GenericRelation('DateAttr',
         related_query_name="%(class)s",
+        help_text="Provide birth or death dates, for example."
         )
     descriptions = GenericRelation('DescriptionAttr',
         related_query_name="%(class)s",
+        help_text="Provide from narrative sources."
         )
 
     def __str__(self):
@@ -52,16 +62,28 @@ class Item(Base):
 # # #
 
 class Source(Base):
+    '''
+    For Citations.
+    '''
     content_types = (
         ('WEBSITE', 'website'),
         ('BOOK', 'book'),
     )
-    content_type = models.CharField(choices=content_types, max_length=50)
+    content_type = models.CharField(choices=content_types,
+        max_length=50,
+        help_text="Select a Work model class referenced by this citation."
+    )
 
     # TODO: refactor to use ContentType framework
 
-    book = models.ForeignKey('Book', blank=True)
-    website = models.ForeignKey('Website', blank=True)
+    book = models.ForeignKey('Book',
+        blank=True,
+        help_text="Select if source is a book.",
+    )
+    website = models.ForeignKey('Website',
+        blank=True,
+        help_text="Select if source is a website.",
+    )
 
     # TODO: needs validation to require at least one foreign key.
 
@@ -70,6 +92,9 @@ class Source(Base):
 
 
 class License(Item):
+    '''
+    For software and other custom copyrights.
+    '''
     name = models.CharField(max_length=200)
     url = models.URLField()
 
@@ -78,65 +103,121 @@ class License(Item):
 
 
 class Color(Item):
+    '''
+    A simple class for storing colors.
+    '''
     name = models.CharField(max_length=200)
-    hue = models.PositiveIntegerField()
-    saturation = models.PositiveIntegerField()
-    lightness = models.PositiveIntegerField()
-    alpha = models.DecimalField(max_digits=3, decimal_places=2 )
+    h = models.PositiveIntegerField(
+        help_text="Select a Hue from 0-255.",
+        default=0,
+    )
+    s = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        help_text="Select a Saturation as a decimal percentage.",
+        default=0.00,
+    )
+    l = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        help_text="Select a Lightness/Brightness as a decimal percentage.",
+        default=0.00,
+    )
+    a = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        help_text="Select a Alpha/Transparency as a decimal percentage.",
+        default=0.00,
+    )
 
+    # TODO: validate maximum levels; validate decimals >= 0
 
 #
 # Attributes
 # # #
 
 class Attribute(models.Model):
+    '''
+    The base for attached attributes to Items.
+    '''
     class Meta:
         abstract=True
         ordering = ['sequence', 'id']
 
     # fields for presentation
-    sequence = models.PositiveIntegerField(default=0)
+    sequence = models.PositiveIntegerField(
+        default=0,
+        help_text="Provide a display order.",
+        )
 
     # fields for generic relations
     content_type = models.ForeignKey(ContentType,
         on_delete = models.CASCADE,
         default = ContentType.objects.get(
-            app_label="collection", model="organization").pk
+            app_label="collection", model="organization").pk,
+        help_text="Provide the model name for the generic relation.",
         )
-    object_id = models.PositiveIntegerField(blank=True, null=True)
+    object_id = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        help_text="Provide the actual object ID of the generic relation within the table of the Content Type.",
+        )
     content_object = GenericForeignKey('content_type', 'object_id')
 
     # fields for citation
-    source = models.ForeignKey('Source', blank=True, null=True)
-    source_accessed = models.DateField(default=now, blank=True)
+    source = models.ForeignKey('Source',
+        blank=True,
+        null=True,
+        help_text="Provide a citation for this attribute.",
+        )
+    source_accessed = models.DateField(
+        default=now,
+        blank=True,
+        help_text="When was this source consulted.",
+        )
 
     def __str__(self):
         return '{} #{}'.format(self.name, self.sequence)
 
 
 class IdentifierAttr(Attribute):
+    '''
+    Custom Item IDs, like OID and external domain identification.
+    '''
     class Meta:
         verbose_name = 'identifier'
         verbose_name_plural = 'identifiers'
 
-    identifier = models.CharField(max_length=200)
+    identifier = models.CharField(
+        max_length=200,
+        help_text="An identifier from the original context, like a DOI, LoC, or museum accession number.",
+        )
 
     def __str__(self):
         return self.identifier
 
 
 class AltNameAttr(Attribute):
+    '''
+    Alternate names, abbreviations, and colloquialisms.
+    '''
     class Meta:
         verbose_name = 'name'
         verbose_name_plural = 'names'
 
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=200,
+        help_text="What other names was the item known by?",
+        )
 
     def __str__(self):
         return self.name
 
 
 class DateAttr(Attribute):
+    '''
+    For tracking birth dates, death dates, and other important date attributes.
+    '''
     class Meta:
         verbose_name = 'date'
         verbose_name_plural = 'dates'
@@ -145,13 +226,15 @@ class DateAttr(Attribute):
     # date = models.DateField(default=now)
 
     categories = (
-        ('START', 'Start'),
-        ('END', 'End'),
+        ('START', 'Birth'),
+        ('END', 'Death'),
     )
+    # TODO: figure out how to alter categories based on related content type.
     category = models.CharField(
         max_length=50,
         choices=categories,
-        default="START"
+        default="START",
+        help_text="What type of date is this?",
     )
 
     def __str__(self):
@@ -159,28 +242,57 @@ class DateAttr(Attribute):
 
 
 class DescriptionAttr(Attribute):
+    '''
+    Descriptions manually crafted or taken from primary sources.
+    '''
     class Meta:
         verbose_name = 'description'
         verbose_name_plural = 'descriptions'
 
-    body = models.TextField()
+    body = models.TextField(
+        help_text="Provide a date snippet from the original context.",
+        )
 
     def __str__(self):
         return self.body[:50]
 
 
 class PlaceAttr(Attribute):
+    '''
+    Basic geographic point data.
+    '''
     class Meta:
         verbose_name = 'place'
         verbose_name_plural = 'places'
 
-    latitude = models.DecimalField(max_digits=10, decimal_places=7)
-    longitude = models.DecimalField(max_digits=10, decimal_places=7)
-    altitude = models.DecimalField(max_digits=10, decimal_places=2)
-    radius = models.DecimalField(max_digits=10, decimal_places=2)
+    latitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        help_text="Provide a latitude of the place centroid.",
+        )
+    longitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=7,
+        help_text="Provide a longitude of the place centroid.",
+        )
+    altitude = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text="Provide an altitude, in meters above sea level.",
+        default=0.0,
+        )
+    radius = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text="Allows a simple approximate area for this point, in meters.",
+        blank=True,
+        )
 
 
 class ImageAttr(Attribute):
+    '''
+    Images.
+    '''
     class Meta:
         verbose_name = 'image'
         verbose_name_plural = 'images'
@@ -191,6 +303,9 @@ class ImageAttr(Attribute):
 
 
 class VersionAttr(Attribute):
+    '''
+    Versions or editions for software and other media.
+    '''
     class Meta:
         verbose_name = 'version'
         verbose_name_plural = 'versions'
@@ -200,27 +315,66 @@ class VersionAttr(Attribute):
 
     pass
 
+# contact_types provide shared categories for phone, email,
+# and addresse attributes
+contact_categories = (
+    ('PRIMARY', 'primary'),
+    ('SECONDARY', 'secondary'),
+    ('WORK', 'work'),
+    ('HOME', 'home'),
+    ('INFODESK', 'main information desk'),
+)
 
 class PhoneAttr(Attribute):
+    '''
+    Phone numbers.
+    '''
     class Meta:
         verbose_name = 'phone'
         verbose_name_plural = 'phones'
+
+    category = models.CharField(
+        max_length=50,
+        choices=contact_categories,
+        help_text="What type of phone number is this?",
+        default="PRIMARY",
+        )
 
     phone = models.CharField(max_length=200)
 
 
 class EmailAttr(Attribute):
+    '''
+    Email addresses.
+    '''
     class Meta:
         verbose_name = 'email'
         verbose_name_plural = 'emails'
+
+    category = models.CharField(
+        max_length=50,
+        choices=contact_categories,
+        help_text="What type of email address is this?",
+        default="PRIMARY",
+        )
 
     email = models.CharField(max_length=200)
 
 
 class AddressAttr(Attribute):
+    '''
+    Postal addresses.
+    '''
     class Meta:
         verbose_name = 'phone'
         verbose_name_plural = 'phones'
+
+    category = models.CharField(
+        max_length=50,
+        choices=contact_categories,
+        help_text="What type of address is this?",
+        default="PRIMARY",
+        )
 
     street = models.TextField()
     city = models.CharField(max_length=100)
@@ -234,6 +388,11 @@ class AddressAttr(Attribute):
 # # #
 
 class Entity(Item):
+    '''
+    Abstract shared characteristics between Organization and Person, because,
+    you know, corporations are people too.
+    TODO: delete?
+    '''
     class Meta:
         abstract = True
 
@@ -241,6 +400,9 @@ class Entity(Item):
 
 
 class Organization(Entity):
+    '''
+    Companies, Communities, Nonprofits, Consortia.
+    '''
     name = models.CharField(max_length=200)
 
     related_organizations = models.ManyToManyField('Organization',
@@ -261,6 +423,9 @@ class Organization(Entity):
 
 
 class OrgOrgRelationship(models.Model):
+    '''
+    Construction for an organization web.
+    '''
     class Meta:
         verbose_name='organization relationship'
 
@@ -287,6 +452,9 @@ class OrgOrgRelationship(models.Model):
 
 
 class OrgPersonRelationship(models.Model):
+    '''
+    Allows People to be part of Organizations.
+    '''
     class Meta:
         verbose_name='member'
 
@@ -311,6 +479,9 @@ class OrgPersonRelationship(models.Model):
 
 
 class Person(Entity):
+    '''
+    A dude or dudette.
+    '''
     class Meta:
         verbose_name_plural = 'people'
 
@@ -330,6 +501,9 @@ class Person(Entity):
 
 
 class Work(Item):
+    '''
+    A created artifact, digital or analog, real or imagined.
+    '''
     class Meta:
         abstract = True
 
@@ -340,18 +514,31 @@ class Work(Item):
 
 
 class Book(Work):
+    '''
+    Your basic codex form.
+    '''
     pass
 
 
 class Website(Work):
+    '''
+    Software or documents accessible via the Web.
+    '''
     url = models.URLField() # the root URL
 
 
 class Tool(Work):
+    '''
+    Digital(?) projects used by LAMs.
+    '''
     license = models.ForeignKey('License') # the root URL
 
 
 class Photograph(Work):
+    '''
+    Distinct from Image, a Photograph is a representation of a physical object
+    in a LAM's collection.
+    '''
     pass
 
 
--- collection/wagtail_hooks.py
+++ collection/wagtail_hooks.py
@@ -1,19 +1,44 @@
 from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, modeladmin_register)
-from .models import Organization
+    ModelAdmin,
+    modeladmin_register,
+)
+from .models import Organization, Person
 
 
 class OrganizationAdmin(ModelAdmin):
     model = Organization
-    menu_icon = 'date'  # change as required
-    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    list_display = ('name', 'related_organizations', 'related_people', 'slug',
-        # 'dates',
-        # 'descriptions'
-        )
-    list_filter = ('name',)
+    menu_icon = 'date'
+    menu_order = 200
+    add_to_settings_menu = False
+    list_display = ('name', 'slug', )
+    # prepopulated_fields = {'slug': ['name']} # TODO: does not work yet
     search_fields = ('name',)
+    # inlines = [
+    #     IdentifierAttrInline,
+    #     AltNameAttrInline,
+    #     DateAttrInline,
+    #     DescriptionAttrInline,
+    #     OrgChildInline,
+    #     OrgParentInline,
+    #     OrgChildPersonInline,
+    # ]
+
+
+class PersonAdmin(ModelAdmin):
+    model = Person
+    menu_icon = 'date'
+    menu_order = 200
+    add_to_settings_menu = False
+    list_display = ('name_last', 'name_first')
+    search_fields = ('name_last', 'name_first')
+    # prepopulated_fields = {'slug': ['name_last', 'name_first']} # TODO
+    # inlines = [
+    #     DateAttrInline,
+    #     DescriptionAttrInline,
+    #     PersonParentOrgInline,
+    # ]
+
 
 # Now you just need to register your customised ModelAdmin class with Wagtail
 modeladmin_register(OrganizationAdmin)
+modeladmin_register(PersonAdmin)
