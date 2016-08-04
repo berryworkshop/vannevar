780031573b794118f95cdf2e61a9a32058e7df95
Allan Berry <allan.berry@gmail.com>
2016-08-04 15:15:23 -0500

    Tests pass.

--- cms/tests.py
+++ cms/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from wagtail.tests.utils import WagtailPageTests
+from django.test import Client
+from wagtail.wagtailcore.models import Page 
+from .models import ArticlePage, ArticleIndexPage, HomePage
 
-# Create your tests here.
+class MyPageTests(WagtailPageTests):
+
+    def setUp(self):
+        '''
+        # Create a test client.
+        '''
+        self.client = Client()
+        self.response = self.client.get('/')
+
+        # self.root_page = Page.get_first_root_node()
+        self.home_page = Page.objects.get(slug='home')
+
+    def test_can_create_a_page(self):
+        # Create a page instance with all the required fields populated,
+        # but not yet saved to the database 
+        page = ArticlePage(
+            title="Test page.",
+            slug="test-page",
+            ) 
+        self.home_page.add_child(instance=page) 
+
+        response = self.client.get('/test-page/')
+        self.assertEqual(response.status_code, 200)
