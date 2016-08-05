156774f434b05052783d8cb0407eb6ca72531f5d
Allan Berry <allan.berry@gmail.com>
2016-08-05 11:40:29 -0500

    Stub in home page.

--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import ArticleView, ArticlesView
+from .views import HomeView, ArticleView, ArticlesView
 
 urlpatterns = [
     url(r'^articles/$', ArticlesView.as_view()),
+    url(r'', HomeView.as_view()),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 
 
+class HomeView(TemplateView):
+    template_name = "cms/home.html"
+
+
 class ArticleView(TemplateView):
     template_name = "cms/article.html"
 
