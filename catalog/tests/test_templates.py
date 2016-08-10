b48addb24823ebf3c6c56128b5c23e78a781059f
Allan Berry <allan.berry@gmail.com>
2016-08-10 07:57:47 -0500

    JSON tests; also refactored tests.

--- catalog/tests/test_templates.py
+++ catalog/tests/test_templates.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test import Client
 from catalog.models import Organization
 from model_mommy import mommy
+import json
 
 
 class HomeTest(TestCase):
@@ -42,44 +43,74 @@ class CatalogOrganizationsTest(TestCase):
         client = Client()
         self.response = self.client.get('/catalog/organizations/')
 
-    def test_orgs_index(self):
+    def test_page(self):
         '''
-        Index page should load.
+        Index page should load and use the correct template.
         '''
         self.assertEqual(self.response.status_code, 200)
-
-    def test_orgs_index(self):
-        '''
-        Response should use correct template.
-        '''
         self.assertTemplateUsed(self.response, 'catalog/organizations.html')
 
-    def test_org_pages(self):
+    def test_json(self):
+        '''
+        Organizations index json pages should load, be JSON, and be longer than a single record.
+        '''
+        response = self.client.get('/catalog/organizations/?format=json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json())
+        self.assertTrue(len(response.json()) > 1)
+
+    # def test_org_not_same_as_json(self):
+    #     '''
+    #     Organization.json pages should not be the same as regular Org pages.
+    #     '''
+    #     org = Organization.objects.get(slug="artic")
+    #     response = self.client.get('/catalog/organizations/?format=json')
+    #     response_incorrect = self.client.get('/catalog/organizations/')
+    #     self.assertNotEqual(response.rendered_content, response_incorrect.rendered_content)
+
+class CatalogSingleOrganizationTest(TestCase):
+
+    def setUp(self):
+        '''
+        Create some organizations to use in tests, and a test client.
+        '''
+        self.orgs = [
+            # provide several to choose from
+            {'name': 'The Art Institute of Chicago', 'slug': 'artic'},
+            {'name': 'The University of Illinois at Chicago', 'slug': 'uic'},
+            {'name': 'Online Computer Library Center, Inc.', 'slug': 'oclc'},
+        ]
+        for org in self.orgs:
+            mommy.make(Organization, name=org['name'], slug=org['slug'])
+
+        client = Client()
+        self.org = Organization.objects.get(slug="artic")
+        self.response = self.client.get(
+            '/catalog/organizations/{}'.format(self.org.slug))
+
+    def test_page(self):
         '''
         Individual Organization pages should load.
         '''
         orgs = Organization.objects.all()
         for org in orgs:
-            response = self.client.get('/catalog/organizations/{}'.format(org.slug))
             try:
-                self.assertEqual(response.status_code, 200)
+                self.assertEqual(self.response.status_code, 200)
             except AssertionError as e:
                 raise AssertionError(
-                    '{}: "/catalog/organizations/{}/"'.format(e, org.slug))
+                    '{}: "/catalog/organizations/{}/"'.format(e, self.org.slug))
 
-    def test_org_page_templates(self):
+    def test_templates(self):
         '''
         Individual Organization pages should use the organization template.
         '''
-        org = Organization.objects.get(slug="artic")
-        response = self.client.get('/catalog/organizations/{}'.format(org.slug))
-        self.assertTemplateUsed(response, 'catalog/organization.html')
+        self.assertTemplateUsed(self.response, 'catalog/organization.html')
 
-    def test_org_json(self):
+    def test_json(self):
         '''
-        Organization.json pages should load and use the JSON API.
+        Single Organization.json pages should load, be JSON, and be a single record.
         '''
-        org = Organization.objects.get(slug="artic")
-        response = self.client.get('/catalog/organizations/{}?format=json'.format(org.slug))
-        self.assertEqual(response.status_code, 200)
-
+        json_response = self.client.get('/catalog/organizations/{}?format=json'.format(self.org.slug))
+        self.assertEqual(json_response.status_code, 200)
+        self.assertTrue(json_response.json())
+        self.assertTrue(len(json_response.json()) == 1)
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpResponse
 from django.core import serializers
+from django.views.generic.base import View
 from django.views.generic import DetailView, ListView
 from .models import Organization, OrgOrgRelationship
 
-class OrganizationView(DetailView):
+class BasicJsonMixin(View):
+    pass
+
+class OrganizationView(BasicJsonMixin, DetailView):
     '''
     Front-facing view for a single Organization.
     '''
     template_name = "catalog/organization.html"
     model = Organization
+
+    def get(self, request, *args, **kwargs):
+        '''
+        Override get to allow json response
+        '''
+        if request.GET.get('format') == 'json':
+            data = serializers.serialize('json', [self.get_object()])
+            return HttpResponse(data, content_type='application/json')
+        else:
+            return super().get(request, *args, **kwargs)
+
 
 
 class OrganizationsView(ListView):
